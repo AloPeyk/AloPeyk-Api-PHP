51334f5654fa1147f3d5997440a50eaa0a0b334e
Arian Shahrizad <a.shahrizad@alopeyk.com>
2018-06-12 17:02:18 +0430

    Change to sandbox on testing environments

--- src/Config/Configs.php
+++ src/Config/Configs.php
@@ -22,9 +22,8 @@ class Configs
     | Don't edit following values
     |
     */
-    const URL = 'https://api.staging.alopeyk.com/';
-    const API_URL = 'https://api.staging.alopeyk.com/api/v2/';
-    const TRACKING_URL = 'https://tracking.staging.alopeyk.com/';
+    const URL = 'https://sandbox-api.alopeyk.com/';
+    const API_URL = 'https://sandbox-api.alopeyk.com/api/v2/';
     const PAYMENT_ROUTES = [
         'saman' => 'payments/saman/checkout',
         'zarinpal' => 'payments/zarinpal/checkout'
